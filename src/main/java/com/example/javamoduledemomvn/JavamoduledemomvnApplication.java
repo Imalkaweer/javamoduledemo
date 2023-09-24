7cbe16294c9732073e3398f900d435c2d1ac8130
imalkaweerasekara <imajavaweerasekara128@gmail.com>
2023-09-24 16:12:12 +0530

    Initial Commit

---
+++ src/main/java/com/example/javamoduledemomvn/JavamoduledemomvnApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamoduledemomvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamoduledemomvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavamoduledemomvnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javamoduledemomvn/JavamoduledemomvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamoduledemomvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamoduledemomvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
